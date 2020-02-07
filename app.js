f15ccadfc4476332729cd56352fe9f777b03b200
Ram <ram2091999@users.noreply.github.com>
2020-02-07 21:42:19 +0530

    Add node-nlp

--- app.js
+++ app.js
@@ -2,6 +2,36 @@
 const express= require("express");
 const app=express();
 const routes= require("./routes/index.js");
+const { NlpManager } = require('node-nlp');
+
+const manager = new NlpManager({ languages: ['en'] });
+// Adds the utterances and intents for the NLP
+manager.addDocument('en', 'goodbye for now', 'greetings.bye');
+manager.addDocument('en', 'bye bye take care', 'greetings.bye');
+manager.addDocument('en', 'okay see you later', 'greetings.bye');
+manager.addDocument('en', 'bye for now', 'greetings.bye');
+manager.addDocument('en', 'i must go', 'greetings.bye');
+manager.addDocument('en', 'hello', 'greetings.hello');
+manager.addDocument('en', 'hi', 'greetings.hello');
+manager.addDocument('en', 'howdy', 'greetings.hello');
+manager.addDocument('en','some shit','some shit');
+
+
+// Train also the NLG
+manager.addAnswer('en', 'greetings.bye', 'Till next time');
+manager.addAnswer('en', 'greetings.bye', 'see you soon!');
+manager.addAnswer('en', 'greetings.hello', 'Hey there!');
+manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+
+// Train and save the model.
+(async() => {
+    await manager.train();
+    manager.save();
+    const response = await manager.process('en', 'khfehrufuhu');
+    console.log(response.intent);
+})();
+
+
 
 
 app.use("/",routes);
